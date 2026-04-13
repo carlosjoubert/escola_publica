c832d9ff859826f63e8576ef1e0c0c944d518056
Carlos Joubert <carlosjoubert@gmail.com>
2026-04-13 09:17:29 -0300

    competado o crud de ProfessorController.java

--- src/main/java/com/cjrock/geo/controller/ProfessorController.java
+++ src/main/java/com/cjrock/geo/controller/ProfessorController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/professores")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -16,12 +18,53 @@ public class ProfessorController {
     private ProfessorRepository repository;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> cadastrarProfessor (@RequestBody Professor professor){
-        if(repository.findByEmail(professor.getEmail()).isPresent()){
+    public ResponseEntity<?> cadastrarProfessor(@RequestBody Professor professor) {
+        if (repository.findByEmail(professor.getEmail()).isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("E-mail já em uso!");
         }
         Professor novoProfessor = repository.save(professor);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoProfessor);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Professor>> listarTodos() {
+        List<Professor> professores = repository.findAll();
+        return ResponseEntity.ok(professores);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Professor> buscarPorId(@PathVariable Long id) {
+        Professor professor = repository.findById(id).orElse(null);
+        if (professor != null) {
+            return ResponseEntity.ok(professor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Professor> atualizarPorId(@PathVariable Long id, @RequestBody Professor professorAtualizado) {
+        Professor professor = repository.findById(id).orElse(null);
+        if (professor != null) {
+            professor.setNome(professorAtualizado.getNome());
+            professor.setEmail(professorAtualizado.getEmail());
+            professor.setSenha(professorAtualizado.getSenha());
+            repository.save(professor);
+            return ResponseEntity.ok(professor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
+        Professor professor = repository.findById(id).orElse(null);
+        if (professor != null) {
+            repository.delete(professor);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
